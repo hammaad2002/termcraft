e1a8f4093c70f736c71056b3bc262e1fc333dc3d
hammaad2002 <hammadalikhan2002@gmail.com>
2025-05-08 03:57:06 +0500

    Add apt dependency in the install script

--- install.sh
+++ install.sh
@@ -10,6 +10,10 @@ ln -sf $(pwd)/tmux/.tmux.conf ~/.tmux.conf
 ln -sf $(pwd)/vim/.vimrc ~/.vimrc
 ln -sf $(pwd)/gnome/.gnome-terminal-config ~/.gnome-terminal-config
 
+
+echo "Installing dependencies ..."
+sudo apt install lm-sensors
+
 echo "[*] Done!"
 
 echo "[*] Applying the changes ..."
